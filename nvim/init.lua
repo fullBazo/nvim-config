5855b755fbaec3235019a7fcd79432537a3faa07
fullBazo <ethan.huet03@gmail.com>
2026-01-09 20:11:42 +0100

    pas finie

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require "conf"
+
+vim.opt.expandtab = false  -- vrais tabs
+vim.opt.tabstop = 4        -- largeur visuelle
+vim.opt.shiftwidth = 4     -- indentation
+vim.opt.softtabstop = 0    -- pas de tabs "fake"
+
+
