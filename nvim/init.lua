5f7c311ce01b6987f332d94be3c80f4e309eab24
Ethan Huet <ehuet@f6r9s2.clusters.42paris.fr>
2026-01-10 20:45:12 +0100

    simple

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,20 @@
-require "conf"
+require('config.options')
+require('config.keybinds')
+--require('config.lazy')
+-- init.lua
+vim.opt.runtimepath:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
-vim.opt.expandtab = false  -- vrais tabs
-vim.opt.tabstop = 4        -- largeur visuelle
-vim.opt.shiftwidth = 4     -- indentation
-vim.opt.softtabstop = 0    -- pas de tabs "fake"
-
+require("lazy").setup({
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup{
+                ensure_installed = { "lua", "python" },
+                highlight = { enable = true },
+                indent = { enable = true },
+            }
+        end
+    }
+})
 
