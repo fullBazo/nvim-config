41539da7da55a5a5f4c67df93c4c2bfa2bea10a6
Ethan Huet <ehuet@f6r9s4.clusters.42paris.fr>
2026-02-18 14:51:00 +0100

    changed

--- nvim/plugin/autocmp.lua
+++
@@ -1,6 +0,0 @@
-require("nvim-autopairs").setup({
-    check_ts = true,  -- support treesitter
-    fast_wrap = {
-        map = "<M-e>",  -- Alt+e pour wrapper du texte avec des parenthèses
-    },
-})
--- nvim/plugin/lsp_signature.lua
+++
@@ -1,9 +0,0 @@
-require("lsp_signature").setup({
-    bind = true,
-    hint_enable = true,
-    hint_prefix = "→ ",
-    floating_window = true,
-    handler_opts = {
-        border = "rounded",
-    },
-})
